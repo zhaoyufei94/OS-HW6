f1d5d8cbbf3e0199cbbf2fe68a6b112b65bc17c8
Yufei Zhao <yz3170@columbia.edu>
2017-12-12 12:42:13 -0500

    sys_call(get_gps)

---
+++ user/gps_d/get.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include "gpsd.h"
+
+int main(int argc, char *argv[])
+{
+	struct gps_location gps;
+	int age;
+	const char *path;
+	
+	if (argc > 1) {
+		path = argv[1];
+		printf("path: %s\n", path);
+	} else {
+		return 0;
+	}
+
+	age = syscall(246, path, &gps);
+	if (age < 0) {
+		printf("error: %s\n", strerror(errno));
+		return 0;
+	} else {
+		printf("age: %d\n", age);
+		return 0;
+	}
+
+}
